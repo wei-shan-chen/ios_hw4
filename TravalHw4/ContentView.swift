10b3ddc4c205f0b3d82025f628c636313f4a7c6d
wei shan <ycc7688@gmail.com>
2020-11-19 16:21:47 +0800

    Initial Commit

---
+++ TravalHw4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravalHw4
+//
+//  Created by 葉之霆 on 2020/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravalHw4/TravalHw4App.swift
@@ -0,0 +1,17 @@
+//
+//  TravalHw4App.swift
+//  TravalHw4
+//
+//  Created by 葉之霆 on 2020/11/19.
+//
+
+import SwiftUI
+
+@main
+struct TravalHw4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
